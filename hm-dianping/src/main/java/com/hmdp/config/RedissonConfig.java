14823cc1d92ae81b51e06806eb8de13a0a855609
SUIFENGQISHI <15287213+SUIFENGQISHI@user.noreply.gitee.com>
2026-02-04 13:53:15 +0800

    连锁测试

--- hm-dianping/src/main/java/com/hmdp/config/RedissonConfig.java
+++ hm-dianping/src/main/java/com/hmdp/config/RedissonConfig.java
@@ -17,4 +17,23 @@ public class RedissonConfig {
         //创建Redisson对象
         return Redisson.create(config);
     }
+
+    @Bean
+    public RedissonClient redissonClient2() {
+        //配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://124.222.230.147:6380").setPassword("Nzzwd050924.");
+        //创建Redisson对象
+        return Redisson.create(config);
+    }
+
+
+    @Bean
+    public RedissonClient redissonClient3() {
+        //配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://124.222.230.147:6381").setPassword("Nzzwd050924.");
+        //创建Redisson对象
+        return Redisson.create(config);
+    }
 }
